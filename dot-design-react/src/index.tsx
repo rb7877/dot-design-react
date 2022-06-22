6879ab8111ecf76bda447d53bc7882cc2c37ccd7
rb7877 <jitendra.appic@gmail.com>
2022-06-22 09:56:51 +0530

    21-6-2022 latest

--- dot-design-react/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { GlobalContextProvider } from './store/global-context';
-
-ReactDOM.render( <GlobalContextProvider>
-    <App/>
-  </GlobalContextProvider>, document.getElementById("root"));
